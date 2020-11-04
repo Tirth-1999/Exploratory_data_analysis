6353736d8c68e7b6420e4816bb0ce951c60e25f3
TIRTH SHAH <tirthdhara108@gmail.com>
2020-11-04 23:00:24 +0530

    first commit

---
+++ DATA SCIENCE(EDA-3)/EDA-3.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+lap_db = pd.read_csv(r"C:\Users\Chandrika Jaikishan\Desktop\TIRTH\DATA SCIENCE\PROJECTS\DATA SCIENCE(EDA-3)\Laptops.csv")
+print(lap_db['brand','laptop_name'])
